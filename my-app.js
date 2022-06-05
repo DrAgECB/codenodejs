d9a6a004ba9cb479c7418990865c54f2d32b0731
DrAgECB <71475911+DrAgECB@users.noreply.github.com>
2022-06-05 07:32:38 +0530

    Update my-app.js

--- my-app.js
+++ my-app.js
@@ -1,13 +1,7 @@
 class ShoppingList extends React.Component {
   render() {
     return (
-      <div className="shopping-list">
-        <h1>Shopping List for {this.props.name}</h1>
-        <ul>
-          <li>Instagram</li>
-          <li>WhatsApp</li>
-          <li>Oculus</li>
-        </ul>
+      <div Hello World!
       </div>
     );
   }
